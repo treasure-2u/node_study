96328959fa6a3cf3c879653be607024e5c718377
treasure-2u <dldbwls1335@gmail.com>
2024-08-22 17:02:55 +0900

    node.js study: REPL mode 주석 첨가

--- 001_hello_world.js
+++ 001_hello_world.js
@@ -1,1 +1,12 @@
+// REPL mode(Read Evaluation Print Loop)
+// 코드를 즉석에서 바로 실행하여 그 결과를 확인할 수 있는 환경
+// terminal > node > console.log("hello world");
+
+// terminal > .editor => 여러 줄의 코드 작성 가능
+// terminal > .editor > console.time("start");
+// for(let i=0; i<10; i++) {
+// console.log("i");}
+// console.timeEnd("start");
+// result = 0~9 찍히고 얼마의 시간이 걸렸는지 확인 가능
+
 console.log("hello world");
