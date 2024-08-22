c2efce9bb58dc8b9b317664bfad07d648c267366
treasure-2u <dldbwls1335@gmail.com>
2024-08-22 16:26:39 +0900

    node.js study: server 구축 test 주석 추가

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
-const http = require("http");
+// 서버 구축
+// 서버 객체 생성
+const http = require("http"); // default module 가져오기
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200, { "Content-Type": "text/plain" });
-  res.write("hello node.js");
-  res.end();
+  res.writeHead(200, { "Content-Type": "text/plain" }); // response 작성
+  res.write("hello node.js"); // response message
+  res.end(); // response 완료
 });
 
+// 서버가 응답할 수 있는 상태로 만들기
 server.listen(3000, () => {
-  console.log("Server is listening on port 3000");
+  console.log("Server is listening on port 3000"); // 동작 완료 메시지 출력
 });
