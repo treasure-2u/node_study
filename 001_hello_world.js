a8e65045486d4fb77fd8983fc885acaf9a0e5abd
treasure-2u <dldbwls1335@gmail.com>
2024-08-22 17:05:02 +0900

    node.js study: node.js 실행 js file mode 주석 첨부

--- 001_hello_world.js
+++ 001_hello_world.js
@@ -1,4 +1,5 @@
-// REPL mode(Read Evaluation Print Loop)
+// node.js 실행
+// 1. REPL mode(Read Evaluation Print Loop)
 // 코드를 즉석에서 바로 실행하여 그 결과를 확인할 수 있는 환경
 // terminal > node > console.log("hello world");
 
@@ -9,4 +10,6 @@
 // console.timeEnd("start");
 // result = 0~9 찍히고 얼마의 시간이 걸렸는지 확인 가능
 
+// 2. js file mode
+// js 파일 생성 > terminal > node fileName.js
 console.log("hello world");
